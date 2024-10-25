a059f4bb80dc3f928cf1815085d61fc4144b20d0
Huseyin Eskan <huseyineskan01@gmail.com>
2024-10-25 23:31:45 +0200

    Project completed.

--- README.md
+++ README.md
@@ -1,13 +1,87 @@
-<<<<<<< HEAD
-# React + Vite
+# Random User Fetch API React
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-=======
-# Random-User-Fetch-API---React
 Random user fetch API react project
->>>>>>> 6d06ea6459b476e294371ac710978b54aee52d81
+
+## Description
+
+The project uses the free API https://randomuser.me/api/. A new random user is drawn with the "New User" button added to the interface.
+
+## Screenshot
+
+![Uygulama Ekran Görüntüsü](https://github.com/huseyineskan/Rock-Paper-Scissors-Game/blob/main/screenshot.png)
+
+### Features that can be added
+
+- With certain filters, users who meet the desired criteria can be attracted.
+- Depending on the person's country, flag colors can be reflected in the design.
+
+### Example Response
+
+```
+{
+    "results": [
+        {
+            "gender": "male",
+            "name": {
+                "title": "Monsieur",
+                "first": "Burim",
+                "last": "Roger"
+            },
+            "location": {
+                "street": {
+                    "number": 57,
+                    "name": "Rue Barrème"
+                },
+                "city": "Astano",
+                "state": "Appenzell Innerrhoden",
+                "country": "Switzerland",
+                "postcode": 9445,
+                "coordinates": {
+                    "latitude": "-63.2601",
+                    "longitude": "-178.0261"
+                },
+                "timezone": {
+                    "offset": "+5:45",
+                    "description": "Kathmandu"
+                }
+            },
+            "email": "burim.roger@example.com",
+            "login": {
+                "uuid": "2eb06017-441a-40aa-92c6-bcc6fd91ec17",
+                "username": "silvergorilla724",
+                "password": "blazers",
+                "salt": "sreNaMO2",
+                "md5": "e1c670b7117910f8ed47746c97dc2b97",
+                "sha1": "2f72c1625df3c2262b807fe6fb76e84e4e8cebfd",
+                "sha256": "1f61d11f62030577652103d966ffb8944beb65dcfc6354b24e76bcbcef1dcb96"
+            },
+            "dob": {
+                "date": "1988-12-11T08:58:02.239Z",
+                "age": 35
+            },
+            "registered": {
+                "date": "2018-03-26T23:18:56.120Z",
+                "age": 6
+            },
+            "phone": "079 618 81 41",
+            "cell": "075 766 17 32",
+            "id": {
+                "name": "AVS",
+                "value": "756.5307.7585.50"
+            },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/40.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/40.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/40.jpg"
+            },
+            "nat": "CH"
+        }
+    ],
+    "info": {
+        "seed": "986bbc7e15af6f7f",
+        "results": 1,
+        "page": 1,
+        "version": "1.4"
+    }
+}
+```
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState(null);
+  const [newUser, setNewUser] = useState(null);
 
   useEffect(() => {
     fetch("https://randomuser.me/api/")
@@ -12,23 +13,128 @@ function App() {
         setData(data.results[0]); // İlk kullanıcıyı alıyoruz
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [newUser]);
 
   return (
-    <div>
-      {data ? (
-        <div>
-          <h2>
-            {data.name
-              ? data.name.first + " " + data.name.last
-              : "ID mevcut değil"}
-          </h2>{" "}
-          {/* Null kontrolü */}
+    <section className="vh-100 user-section">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-lg-10 mb-4 mb-lg-0">
+            <div className="card mb-3" style={{ borderRadius: ".5rem" }}>
+              {data ? (
+                <div className="row g-0">
+                  <div
+                    className="col-md-4 gradient-custom text-center text-white"
+                    style={{
+                      borderTopLeftRadius: ".5rem",
+                      borderBottomLeftRadius: ".5rem",
+                    }}
+                  >
+                    {data.picture ? (
+                      <img
+                        src={data.picture.large}
+                        alt={data.name.first + " " + data.name.last}
+                        className="rounded-circle mb-2 user-picture"
+                      />
+                    ) : (
+                      "Not found"
+                    )}
+                    <h5>
+                      {data.name
+                        ? data.name.first + " " + data.name.last
+                        : "Not found"}
+                    </h5>
+                    <p>{data.name.title ? data.name.title : "Not found"}</p>
+                    <i className="far fa-edit mb-5" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body p-4">
+                      <h6 className="d-flex justify-content-between align-items-center">
+                        User Information{" "}
+                        <button
+                          className="get-new-user-btn"
+                          onClick={setNewUser}
+                        >
+                          New User
+                        </button>
+                      </h6>
+                      <hr className="mt-0 mb-4" />
+                      <div className="row pt-1">
+                        <div className="col-12 mb-3">
+                          <h6>Address</h6>
+                          <p className="text-muted">
+                            {data.location
+                              ? data.location.street.name +
+                                ", " +
+                                data.location.city +
+                                ", " +
+                                data.location.state +
+                                ", " +
+                                data.location.postcode +
+                                ", " +
+                                data.location.country
+                              : "Not found"}
+                          </p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Phone</h6>
+                          <p className="text-muted">
+                            {data.phone ? data.phone : "Not found"}
+                          </p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Cell</h6>
+                          <p className="text-muted">
+                            {data.cell ? data.cell : "Not found"}
+                          </p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Email</h6>
+                          <p className="text-muted">
+                            {data.email ? data.email : "Not found"}
+                          </p>
+                        </div>
+                      </div>
+                      <h6>Login Information</h6>
+                      <hr className="mt-0 mb-4" />
+                      <div className="row pt-1">
+                        <div className="col-6 mb-3">
+                          <h6>Username</h6>
+                          <p className="text-muted">
+                            {data.login ? data.login.username : "Not found"}
+                          </p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Password</h6>
+                          <p className="text-muted">
+                            {data.login ? data.login.password : "Not found"}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-start">
+                        <a href="#!">
+                          <i className="fab fa-facebook-f fa-lg me-3" />
+                        </a>
+                        <a href="#!">
+                          <i className="fab fa-twitter fa-lg me-3" />
+                        </a>
+                        <a href="#!">
+                          <i className="fab fa-instagram fa-lg" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="user-loading">
+                  <img src="./src/assets/loading.gif" alt="" />
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+      </div>
+    </section>
   );
 }
 
